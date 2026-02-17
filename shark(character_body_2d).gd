a8c86f60eb8ac367fc3de1cef81e568065a64b6c
MLogic <magicianlogician@outlook.com>
2026-02-17 23:22:57 +0800

    this is our proj
    everything

---
+++ shark(character_body_2d).gd
@@ -0,0 +1,70 @@
+extends CharacterBody2D
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@export var speed = 150
+const rotation_speed = 1.5
+
+var rotation_direction = 0
+
+func get_input():
+	rotation_direction = Input.get_axis("left", "right")
+	velocity = transform.x * Input.get_axis("down", "up") * speed
+
+func _physics_process(delta):
+	get_input()
+	rotation += rotation_direction * rotation_speed * delta
+	move_and_slide()
